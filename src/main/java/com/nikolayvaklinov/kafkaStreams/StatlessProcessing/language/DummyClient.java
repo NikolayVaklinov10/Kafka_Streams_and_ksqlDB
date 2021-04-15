79c77fc0c69b0819cd21cc66ad63719f6c177eb2
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-04-15 22:49:21 +0100

    still the fix needed here for the Entity Sentiment

--- src/main/java/com/nikolayvaklinov/kafkaStreams/StatlessProcessing/language/DummyClient.java
+++ src/main/java/com/nikolayvaklinov/kafkaStreams/StatlessProcessing/language/DummyClient.java
@@ -13,27 +13,27 @@ public class DummyClient implements LanguageClient {
         return tweet;
     }
 
-    @Override
-    public List<EntitySentiment> getEntitySentiment(Tweet tweet) {
-        List<EntitySentiment> results = new ArrayList<>();
-
-        Iterable<String> words = Splitter.on(' ').split(tweet.getText().toLowerCase().replace("#", ""));
-        for (String entity : words) {
-            EntitySentiment entitySentiment =
-                    EntitySentiment.newBuilder()
-                            .setCreatedAt(tweet.getCreatedAt())
-                            .setId(tweet.getId())
-                            .setEntity(entity)
-                            .setText(tweet.getText())
-                            .setSalience(randomDouble())
-                            .setSentimentScore(randomDouble())
-                            .setSentimentMagnitude(randomDouble())
-                            .build();
-
-            results.add(entitySentiment);
-        }
-        return results;
-    }
+//    @Override
+//    public List<EntitySentiment> getEntitySentiment(Tweet tweet) {
+//        List<EntitySentiment> results = new ArrayList<>();
+//
+//        Iterable<String> words = Splitter.on(' ').split(tweet.getText().toLowerCase().replace("#", ""));
+//        for (String entity : words) {
+//            EntitySentiment entitySentiment =
+//                    EntitySentiment.newBuilder()
+//                            .setCreatedAt(tweet.getCreatedAt())
+//                            .setId(tweet.getId())
+//                            .setEntity(entity)
+//                            .setText(tweet.getText())
+//                            .setSalience(randomDouble())
+//                            .setSentimentScore(randomDouble())
+//                            .setSentimentMagnitude(randomDouble())
+//                            .build();
+//
+//            results.add(entitySentiment);
+//        }
+//        return results;
+//    }
 
     Double randomDouble() {
         return ThreadLocalRandom.current().nextDouble(0, 1);
