ac22f97ea190fc88fc5b99ceb5fd4af89f2e9a49
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-04-16 14:25:00 +0100

    the following file will be heavily used in the kafka streams application

---
+++ src/main/java/com/nikolayvaklinov/kafkaStreams/StatefulProcessing/serialization/json/JsonSerdes.java
@@ -0,0 +1,44 @@
+package com.nikolayvaklinov.kafkaStreams.StatefulProcessing.serialization.json;
+
+import com.nikolayvaklinov.kafkaStreams.StatefulProcessing.HighScores;
+import com.nikolayvaklinov.kafkaStreams.StatefulProcessing.model.Player;
+import com.nikolayvaklinov.kafkaStreams.StatefulProcessing.model.Product;
+import com.nikolayvaklinov.kafkaStreams.StatefulProcessing.model.ScoreEvent;
+import com.nikolayvaklinov.kafkaStreams.StatefulProcessing.model.join.Enriched;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+
+public class JsonSerdes {
+
+    public static Serde<HighScores> HighScores() {
+        JsonSerializer<HighScores> serializer = new JsonSerializer<>();
+        JsonDeserializer<HighScores> deserializer = new JsonDeserializer<>(HighScores.class);
+        return Serdes.serdeFrom(serializer, deserializer);
+    }
+
+    public static Serde<Enriched> Enriched() {
+        JsonSerializer<Enriched> serializer = new JsonSerializer<>();
+        JsonDeserializer<Enriched> deserializer = new JsonDeserializer<>(Enriched.class);
+        return Serdes.serdeFrom(serializer, deserializer);
+    }
+
+    public static Serde<ScoreEvent> ScoreEvent() {
+        JsonSerializer<ScoreEvent> serializer = new JsonSerializer<>();
+        JsonDeserializer<ScoreEvent> deserializer = new JsonDeserializer<>(ScoreEvent.class);
+        return Serdes.serdeFrom(serializer, deserializer);
+    }
+
+    public static Serde<Player> Player() {
+        JsonSerializer<Player> serializer = new JsonSerializer<>();
+        JsonDeserializer<Player> deserializer = new JsonDeserializer<>(Player.class);
+        return Serdes.serdeFrom(serializer, deserializer);
+    }
+
+    public static Serde<Product> Product() {
+        JsonSerializer<Product> serializer = new JsonSerializer<>();
+        JsonDeserializer<Product> deserializer = new JsonDeserializer<>(Product.class);
+        return Serdes.serdeFrom(serializer, deserializer);
+    }
+
+
+}
