7fd676784f361d621189cdc778a8b4bf65b55c4e
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-04-19 22:06:01 +0100

    the pulse class and the implementation of the Vital interface

---
+++ src/main/java/com/nikolayvaklinov/kafkaStreams/WindowsAndTime/model/Pulse.java
@@ -0,0 +1,9 @@
+package com.nikolayvaklinov.kafkaStreams.WindowsAndTime.model;
+
+public class Pulse implements Vital {
+    private String timestamp;
+
+    public String getTimestamp() {
+        return this.timestamp;
+    }
+}
